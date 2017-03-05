acf6fd04864a977f67fe0f930199f719f84b7bf0
Jakob Russel <cmf483@science.domain>
2017-03-05 13:01:52 +0100

    Added proportionality function

---
+++ R/proportionality.R
@@ -0,0 +1,46 @@
+#' Calculate proportionality between all pairs of OTUs, for network analysis
+#'
+#' Calculate proportionality as proposed by Lovell et al. 2016 Proportionality: a valid alternative to correlation for relative data
+#' @param x An otu-table with OTUs as rows OR a phyloseq object
+#' @param delta The pseudocount to exchange zeroes with. Zero-correction is multiplicative such that the proportionality between any entirely non-zero OTUs will not be affected by the pseudocount. 
+#' @keywords network association
+#' @return The network association matrix with proportionality values
+#' @import phyloseq
+#' @export
+
+proportionality <- function(x,delta=1){
+  
+  if(class(x)!="phyloseq") otu <- x else {
+    if(taxa_are_rows(x)) otu <- otu_table(x)
+    if(!taxa_are_rows(x)) otu <- t(otu_table(x)) 
+  }
+  
+  mat <- matrix(nrow=nrow(otu),ncol=nrow(otu)) 
+  
+  # Zero correction
+  otu_zc <- matrix(nrow=nrow(otu),ncol=ncol(otu))  
+  for(k in 1:ncol(otu)){
+    samp <- as.data.frame(otu[,k])
+    z <- sum(samp==0)
+    col_new <- apply(samp,1,function(x) ifelse(x==0,delta,(1-(z*delta)/sum(samp))*x))
+    otu_zc[,k] <- col_new
+  }
+  
+  # CLR transformation
+  otu_gm <- apply(otu_zc,2,function(y) prod(y)^(1/length(y)))
+  if(any(otu_gm == 0)) warning("Geometric means has zeroes, choose higher delta or trim OTUs")
+  if(any(is.infinite(otu_gm))) warning("Geometric means has infinities, choose lower delta or rarefy")
+  otu_log <- t(log(t(otu_zc)/otu_gm))
+  
+  # Proportionality
+  for(i in 1:nrow(otu)){
+    for(j in 1:nrow(otu)){
+      if(i==j) prop <- 1
+      if(i != j) prop <- (2*cov(otu_log[i,],otu_log[j,]))/(var(otu_log[i,])+var(otu_log[j,]))
+      mat[i,j] <- prop
+    }
+  }
+  rownames(mat) <- rownames(otu)
+  colnames(mat) <- rownames(otu)
+  return(mat)
+}
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ This functions combines rarefaction with normalization. It simply
 rarefies reads with a probability of the inverse 16S rRNA copy number,
 such that besides rarefying the read counts the otu-table will be
 corrected for the varying 16S rRNA copy numbers.
+
+#### proportionality
+
+Calculate proportionality on a phyloseq object or otu-table. Proposed by
+Lovell et al. 2016 Proportionality: a valid alternative to correlation
+for relative data
+(<http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1004075>)
