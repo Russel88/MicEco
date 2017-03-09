6dedd10955e489d3b7c3d80ea3715126882efa18
Jakob Russel <russel2620@gmail.com>
2017-03-09 10:26:35 +0100

    Update rarefy_rrna.R
    Added download of copy number database

--- R/rarefy_rrna.R
+++ R/rarefy_rrna.R
@@ -3,15 +3,15 @@
 #' Rarefy an otu-table with the probability of the inverse 16S rRNA copy numbers: The result is a standardized AND normalized otu-table.
 #' @param x An otu-table with OTUs as columns OR a phyloseq object
 #' @param reads Number of reads to sample. 
-#' @param rRNA A dataframe with two variables: "ID" is the OTU id matched by names in x and "Copy" is the copy number
+#' @param copy.database What Greengenes database version was used to find OTUs. Atm only "v13.5" is available. Alternatively, A dataframe with two variables: "ID" is the OTU id matched by names in x and "Copy" is the copy number.
 #' @param seed Random seed for sampling.
 #' @keywords rarefy normalize rrna
 #' @return A rarefied otu-table
 #' @import phyloseq
 #' @export
 
-rarefy_rrna <- function(x, reads, rRNA, seed=NULL){
-  if(class(x) == "phyloseq") y <- rarefy_rrna.phyloseq(x,reads,rRNA,seed) else y <- rarefy_rrna.matrix(x,reads,rRNA,seed)
+rarefy_rrna <- function(x, reads, copy.database="v13.5", seed=NULL){
+  if(class(x) == "phyloseq") y <- rarefy_rrna.phyloseq(x,reads,copy.database,seed) else y <- rarefy_rrna.matrix(x,reads,copy.database,seed)
   return(y)
 }
 
@@ -24,7 +24,15 @@ rarefy_rrna.matrix <- function (x, reads, rRNA, seed=NULL){
     rand.seed <- sample(1000,1)
     set.seed(rand.seed)
     message(paste("Remember to set seed!","Now set to",rand.seed)) }
-  
+
+  # Load Copy database
+  if(copy.database == "v13.5"){
+    rRNA <- read.table("https://raw.githubusercontent.com/Russel88/MicEco/master/data/gg_13_5_16S.tab")
+    colnames(rRNA) <- c("ID","Copy")
+  } else {
+    rRNA <- copy.database 
+  }
+    
   # Probabilities
   rrna <- as.data.frame(rRNA[rRNA$ID %in% colnames(x),])
   rownames(rrna) <- rrna$ID
@@ -61,6 +69,14 @@ rarefy_rrna.phyloseq <- function (x, reads, rRNA, seed=NULL){
   x2 <- as.matrix(otu_table(x))
   if(taxa_are_rows(x)) x2 <- as.matrix(t(x2))
   
+  # Load Copy database
+  if(copy.database == "v13.5"){
+    rRNA <- read.table("https://raw.githubusercontent.com/Russel88/MicEco/master/data/gg_13_5_16S.tab")
+    colnames(rRNA) <- c("ID","Copy")
+  } else {
+    rRNA <- copy.database 
+  }
+    
   # Probabilities
   rrna <- as.data.frame(rRNA[rRNA$ID %in% colnames(x2),])
   rownames(rrna) <- rrna$ID
