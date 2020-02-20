ae87ef0e7e20c20ccc35785d2b834aadd4ca738a
Jakob Russel <cmf483@science.domain>
2020-02-20 23:31:59 +0100

    add functionality and increase speed

--- R/ps_euler.R
+++ R/ps_euler.R
@@ -3,6 +3,7 @@
 #' Make Euler diagram of shared taxa (ASVs, OTUs) across sample groups from a phyloseq object. Overlap can be weighted by relative abundance
 #' @param ps A phyloseq object
 #' @param group The grouping factor. Should match variable in sample_data(ps)
+#' @param fraction The fraction (0 to 1) of samples in a group in which the taxa should be present to be included in the count.
 #' @param weight If TRUE, the overlaps are weighted by abundance
 #' @param type "percent" or "counts"
 #' @param relative Should abundances be made relative
@@ -14,17 +15,18 @@
 #' @importFrom stats aggregate as.formula
 #' @export
 
-ps_euler <- function(ps, group, weight = FALSE, type = "percent", relative = TRUE, ...){
+ps_euler <- function(ps, group, fraction = 0, weight = FALSE, type = "percent", relative = TRUE, ...){
     
     if(relative){
         ps <- transform_sample_counts(ps, function(x) x/sum(x))
     }
     
-    ps_melted <- psmelt(ps)
+    ps_melted <- reshape2::melt(otu_table(ps))
+    ps_melted <- merge(ps_melted, sample_data(ps), by.x = "Var2", by.y = "row.names")
     
-    ps_agg <- aggregate(as.formula(paste("Abundance ~ OTU +",group)), data = ps_melted, function(x) mean(x))
+    ps_agg <- aggregate(as.formula(paste("value ~ Var1 +",group)), data = ps_melted, function(x) (sum(x > 0)/length(x) >= fraction) * mean(x))
     
-    ps_mat <- reshape2::dcast(as.formula(paste("OTU ~ ",group)), data = ps_agg, value.var = "Abundance")
+    ps_mat <- reshape2::dcast(as.formula(paste("Var1 ~ ",group)), data = ps_agg, value.var = "value")
     
     ps_mat <- ps_mat[, -1]
     ps_mat_bin <- (ps_mat>0)*1
