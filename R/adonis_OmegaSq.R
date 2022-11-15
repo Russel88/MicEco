9b6a0bbc555d84ccd0fdfd535fab7e581acedeef
Jari Oksanen <jhoksane@gmail.com>
2022-11-15 23:12:57 +0200

    vegan::adonis is deprecated: upgrade function to work with adonis2
    This is a minimal edit that will work both with the old deprecated
    vegan::adonis and new supported vegan::adonis2.
    
    This edit was made in response to https://stackoverflow.com/questions/74446879/

--- R/adonis_OmegaSq.R
+++ R/adonis_OmegaSq.R
@@ -1,29 +1,43 @@
 #' Calculate (partial) Omega-squared (effect-size calculation) for PERMANOVA and add it to the input object
-#' 
+#'
 #' @param adonisOutput An adonis object
-#' @param partial Should partial omega-squared be calculated (sample size adjusted). Default TRUE 
+#' @param partial Should partial omega-squared be calculated (sample size adjusted). Default TRUE
 #' @return Original adonis object with the (partial) Omega-squared values added
 #' @import vegan
 #' @export
 adonis_OmegaSq <- function(adonisOutput, partial = TRUE){
-    if(!is(adonisOutput, "adonis")) stop("Input should be an adonis object")
-    aov_tab <- adonisOutput$aov.tab
+    if(!(is(adonisOutput, "adonis") || is(adonisOutput, "anova.cca")))
+        stop("Input should be an adonis object")
+    if (is(adonisOutput, "anova.cca")) {
+        aov_tab <- adonisOutput
+        aov_tab$MeanSqs <- aov_tab$SumOfSqs / aov_tab$Df
+        aov_tab$MeanSqs[length(aov_tab$Df)] <- NA
+    } else {
+        aov_tab <- adonisOutput$aov.tab
+    }
     heading <- attr(aov_tab, "heading")
-    MS_res <- aov_tab[rownames(aov_tab) == "Residuals", "MeanSqs"]
+    MS_res <- aov_tab[pmatch("Residual", rownames(aov_tab)), "MeanSqs"]
     SS_tot <- aov_tab[rownames(aov_tab) == "Total", "SumsOfSqs"]
     N <- aov_tab[rownames(aov_tab) == "Total", "Df"] + 1
     if(partial){
         omega <- apply(aov_tab, 1, function(x) (x["Df"]*(x["MeanSqs"]-MS_res))/(x["Df"]*x["MeanSqs"]+(N-x["Df"])*MS_res))
         aov_tab$parOmegaSq <- c(omega[1:(length(omega)-2)], NA, NA)
-        cn_order <- c("Df", "SumsOfSqs", "MeanSqs", "F.Model", "R2", "parOmegaSq", "Pr(>F)")
     } else {
         omega <- apply(aov_tab, 1, function(x) (x["SumsOfSqs"]-x["Df"]*MS_res)/(SS_tot+MS_res))
         aov_tab$OmegaSq <- c(omega[1:(length(omega)-2)], NA, NA)
-        cn_order <- c("Df", "SumsOfSqs", "MeanSqs", "F.Model", "R2", "OmegaSq", "Pr(>F)")
     }
+    if (is(adonisOutput, "adonis"))
+        cn_order <- c("Df", "SumsOfSqs", "MeanSqs", "F.Model", "R2",
+                      if (partial) "parOmegaSq" else "OmegaSq", "Pr(>F)")
+    else
+        cn_order <- c("Df", "SumOfSqs", "F", if (partial) "parOmegaSq" else "OmegaSq",
+                      "Pr(>F)")
     aov_tab <- aov_tab[, cn_order]
     attr(aov_tab, "names") <- cn_order
     attr(aov_tab, "heading") <- heading
-    adonisOutput$aov.tab <- aov_tab
+    if (is(adonisOutput, "adonis"))
+        adonisOutput$aov.tab <- aov_tab
+    else
+        adonisOutput <- aov_tab
     return(adonisOutput)
 }
